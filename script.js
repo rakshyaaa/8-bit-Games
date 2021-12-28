90cc99c00fc9122c769205a1cbfc949a466e67a0
Rakshya Pandey <prakshya90@gmail.com>
2021-12-28 15:16:39 +0545

    carousel fixec

---
+++ script.js
@@ -0,0 +1,42 @@
+
+function carouselSlide(n) {
+
+    let slides = document.getElementsByClassName("carouselImage");
+    let dots = document.getElementsByClassName("carouselDot");
+    
+    for(let i = 0; i <slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for(let i = 0; i <dots.length; i++) {
+        dots[i].style.backgroundColor = "white";
+    }
+
+    slides[n].style.display = "block";
+    dots[n].style.backgroundColor = "blue";
+
+}
+
+var slideIndex = 0;
+
+function transitionSlides() {
+    let slides = document.getElementsByClassName("carouselImage");
+    let dots = document.getElementsByClassName("carouselDot");
+    
+    for(let i = 0; i <slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for(let i = 0; i <dots.length; i++) {
+        dots[i].style.backgroundColor = "white";
+    }
+
+
+    slideIndex++;
+    if(slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].style.backgroundColor = "blue";
+    setTimeout(transitionSlides, 3000);
+}
+
+transitionSlides();
